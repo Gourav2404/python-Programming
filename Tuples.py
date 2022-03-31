bcde73d412712a92c9940a8c4e780332cd136e5f
Gourav Kadam <gouravkadam49@gmail.com>
2022-03-31 21:21:50 +0530

    Tuples

--- Tuples.py
+++ Tuples.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split())
+    n = int(input())
+    integer_list = map(int, input().split())
     t = tuple(integer_list)
-    print(hash(t));
+    print(hash(t))
