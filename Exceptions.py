b1544a5d6d3b137d8afb50b211db05c18d1ad6bd
Gourav Kadam <gouravkadam49@gmail.com>
2022-04-23 20:14:04 +0530

    Exceptions

---
+++ Exceptions.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+lines = int(input())
+for _ in range(lines):
+    a, b = input().split()
+    try:
+        print(int(a)//int(b))
+    except (ZeroDivisionError, ValueError) as e:
+        print("Error Code:", e)
