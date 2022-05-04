741e6d8bc8d22a3b33b04090fa0fef290dda8e9e
Gourav Kadam <gouravkadam49@gmail.com>
2022-05-04 20:30:33 +0530

    Stair Case

---
+++ Staircase.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+
+def staircase(n):
+    # Write your code here
+    hashs = 1
+    while n > 0:
+        print((n-1)*' ' + hashs*('#'))
+        n = n - 1
+        hashs = hashs + 1
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
