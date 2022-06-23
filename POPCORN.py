ac7de5acd334150301941c8827c130f55c58aef2
Gourav Kadam <gouravkadam49@gmail.com>
2022-06-23 21:40:15 +0530

    POPCORN

---
+++ POPCORN.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    x1,y1=map(int,input().split())
+    x2,y2=map(int,input().split())
+    x3,y3=map(int,input().split())
+    a1=x1+y1
+    a2=x2+y2
+    a3=x3+y3
+    lst=[]
+    lst=[a1,a2,a3]
+    lst.sort()
+    print(lst[2])
