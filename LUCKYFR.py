91d477bbacfc53e83b2af07a63b2d0070858605c
Gourav Kadam <gouravkadam49@gmail.com>
2022-06-26 17:51:13 +0530

    LUCKYFR

---
+++ LUCKYFR.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    fours = 0
+    num = int(input())
+    while(num):
+        if(num%10 == 4):
+            fours +=1
+        num = int(num/10)
+    print(fours)
