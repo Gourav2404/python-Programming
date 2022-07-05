f5e95c48f2f1beb5aeccbedbc94b45644d87aedc
Gourav Kadam <gouravkadam49@gmail.com>
2022-07-05 12:33:09 +0530

    ACCURACY

---
+++ ACCURACY.py
@@ -0,0 +1,13 @@
+# cook your dish here
+# cook your dish here
+a=int(input())
+for r in range(a):
+    b=int(input())
+    c=b%3
+    if c!=0:
+        d=(b//3)+1
+        e=d*3
+        f=e-b
+        print(f)
+    else:
+        print(0)
