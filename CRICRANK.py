ad1db0498ced7e154bd254045846dbac9692e424
Gourav Kadam <gouravkadam49@gmail.com>
2022-08-02 10:12:43 +0530

    ,

--- CRICRANK.py
+++ CRICRANK.py
@@ -10,21 +10,22 @@ for l in range(t):
     
     if(r1>r2):
 	    cntr1+=1
-    elif(r2>r1):
-	    cntr2+=1
+
+    # elif(r2>r1):
+	#     cntr2+=1
 	    
-    if(w1>w2):
-	    cntr1+=1
-    elif(w2>w1):
-	    cntr2+=1
+    # if(w1>w2):
+	#     cntr1+=1
+    # elif(w2>w1):
+	#     cntr2+=1
 	    
-    if(c1>c2):
-	    cntr1+=1
-    elif(c2>c1):
-	    cntr2+=1
+    # if(c1>c2):
+	#     cntr1+=1
+    # elif(c2>c1):
+	#     cntr2+=1
 	    
 	
-    if(cntr1>cntr2):
-	    print("A")
-    elif(cntr2>cntr1):
-	    print("B")
+    # if(cntr1>cntr2):
+	#     print("A")
+    # elif(cntr2>cntr1):
+	#     print("B")
