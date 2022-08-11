64f335c17d9440d0680685a0adef61d4e6b03739
Gourav Kadam <gouravkadam49@gmail.com>
2022-08-11 21:34:41 +0530

    Sales Season

---
+++ SALESEASON.py
@@ -0,0 +1,12 @@
+# cook your dish here
+n = int(input())
+for i in range(n):
+    x = int(input())
+    if x<=100:
+        print(x)
+    elif x>100 and x<=1000:
+        print(x-25)
+    elif x>1000 and x<=5000:
+        print(x-100)
+    elif x>5000:
+        print(x-500)
